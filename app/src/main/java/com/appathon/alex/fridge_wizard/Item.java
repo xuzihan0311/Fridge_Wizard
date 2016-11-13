49222980677439d541e2017c876127e8dd976729
XingxingSun <jsun320@gatech.edu>
2016-11-13 02:41:39 -0500

    xing

---
+++ app/src/main/java/com/appathon/alex/fridge_wizard/Item.java
@@ -0,0 +1,17 @@
+package com.appathon.alex.fridge_wizard;
+
+/**
+ * Created by xingxing on 11/13/16.
+ */
+
+// Items the user has in
+// the fridge like apple, kale and so on
+public class Item {
+    private String name;
+    public Item(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
