50978be6facd4bf0c3ba22c40e46520d65bfd0ce
bigjohnlin <bigjohnlin@gmail.com>
2016-11-13 04:10:09 -0500

    manifest activity update

--- app/src/main/java/controller/recipe_InstructionController.java
+++ app/src/main/java/controller/recipe_InstructionController.java
@@ -1,9 +1,18 @@
 package controller;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.appathon.alex.fridge_wizard.R;
+
 /**
  * Created by xingxing on 11/13/16.
  */
 
-public class recipe_InstructionController {
-
+public class recipe_InstructionController extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recipe_instruction);
+    }
 }
