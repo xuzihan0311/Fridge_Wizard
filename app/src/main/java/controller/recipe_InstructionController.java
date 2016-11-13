ba265d8add8a4d4982c5ae2ea93ecefff52567a9
bigjohnlin <bigjohnlin@gmail.com>
2016-11-13 04:50:09 -0500

    back to fridge button

--- app/src/main/java/controller/recipe_InstructionController.java
+++ app/src/main/java/controller/recipe_InstructionController.java
@@ -1,7 +1,10 @@
 package controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.appathon.alex.fridge_wizard.R;
 
@@ -10,9 +13,19 @@ import com.appathon.alex.fridge_wizard.R;
  */
 
 public class recipe_InstructionController extends AppCompatActivity {
+    Button backToFridge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_instruction);
+
+        backToFridge = (Button) findViewById(R.id.backToFridge);
+        backToFridge.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), FridgeController.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
 }
